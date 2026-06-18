247c354c1a03820c5c9db739178420b12e2d91b8
LumenfieldStudioAI <LumenfieldAI@protonmail.com>
2026-06-18 18:04:21 +0200

    Set Clerk sign-in redirect

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-black">
-      <SignIn />
+      <SignIn signUpUrl="/sign-up" fallbackRedirectUrl="/generate" />
     </div>
   );
 }
